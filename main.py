c54e76bddf14648f8ddb9c7f53dcf2845f2bbef9
Sai Harsha Kantamaneni <harsha.kantamneni148@gmail.com>
2024-03-12 13:01:48 -0400

    comment

--- main.py
+++ main.py
@@ -821,8 +821,8 @@ if __name__ == "__main__":
             except Exception:
                 melk()
                 raise
-        if not opt.no_test and not trainer.interrupted:
-            trainer.test(model, data)
+        # if not opt.no_test and not trainer.interrupted:
+        #     trainer.test(model, data)
     except Exception:
         logging.error('Some exception has occurred')
         if opt.debug and trainer.global_rank == 0:
